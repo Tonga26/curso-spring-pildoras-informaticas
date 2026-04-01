fda128b1393fb294de8a9ce2d3e1a76e91e31707
Gaston Giorgio <gastongiorgio26@gmail.com>
2026-04-01 08:11:57 -0300

    refactor: extract pointcut declaration for reuse and modularize before advices

--- src/es/pildoras/spring/gestionaop/aspectos/LoginConAspecto.java
+++ src/es/pildoras/spring/gestionaop/aspectos/LoginConAspecto.java
@@ -2,15 +2,29 @@ package es.pildoras.spring.gestionaop.aspectos;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoginConAspecto {
 
-    @Before("execution(public * insertaCliente*(..))")
+    @Pointcut("execution(public * insertaCliente*(..))")
+    private void paraClientes(){};
+
+    @Before("paraClientes()")
     public void antesInsertarCliente(){
         System.out.println("El usuario está logueado.");
         System.out.println("El perfil para insertar clientes es correcto.");
+    }
+
+    @Before("paraClientes()")
+    public void requisitosCliente(){
+        System.out.println("El cliente cumple los requisitos para ser insertado en la BBDD.");
+    }
+
+    @Before("paraClientes()")
+    public void requisitosTabla(){
+        System.out.println("Hay menos de 3000 registros en la tabla, puedes insertar el nuevo cliente.");
     }
 }
